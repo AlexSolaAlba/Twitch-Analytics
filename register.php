ba5d7550f26df584566c6796bc9cc2d3bce64523
AlexSolaAlba <149529379+AlexSolaAlba@users.noreply.github.com>
2025-02-13 15:56:13 +0100

    [#5] - Añadido registro de usuario

--- register.php
+++ register.php
@@ -6,7 +6,7 @@
             $email = $_POST['email'];
             if(comprobarEmail($email)){
                 $key = bin2hex(random_bytes(16));
-                #guardarEnBaseDeDatos($email,$key);
+                guardarEnBBDD($email,$key);
                 $message = [
                     'api_key' => $key
                 ];
@@ -36,4 +36,35 @@
     function comprobarEmail($email){
         return preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/",$email);
     }
+
+    function guardarEnBBDD($email, $key) {
+        $conexion = mysqli_connect("db5017192767.hosting-data.io", "dbu2466002", "s9saGODU^mg2SU", "dbs13808365");
+        #$conexion = mysqli_connect("localhost", "root", "", "twitch-analytics");
+    
+        if ($conexion->connect_error) {
+            die("Error de conexión de base de datos: " . $conexion->connect_errno . " - " . $conexion->connect_error);
+        }
+    
+        $consulta = $conexion->prepare("SELECT userID FROM User WHERE userEmail = ?");
+        $consulta->bind_param("s", $email);
+        $consulta->execute();
+        $resultado = $consulta->get_result();
+        $datos = $resultado->fetch_assoc();
+        $consulta->close();
+    
+        if (isset($datos['userID'])) {
+            $id = $datos['userID'];
+            $stmt = $conexion->prepare("UPDATE User SET userApiKey = ? WHERE userID = ?");
+            $stmt->bind_param("si", $key, $id);
+        } else {
+            $stmt = $conexion->prepare("INSERT INTO User (userEmail, userApiKey) VALUES (?, ?)");
+            $stmt->bind_param("ss", $email, $key);
+        }
+    
+        $stmt->execute();
+    
+        $stmt->close();
+        $conexion->close();
+    }
+    
 ?>
