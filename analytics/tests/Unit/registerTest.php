f4f6849873b50f2149cea6ee65c5bb3a2b4d55a4
OierBaigo <oierbaigo@gmail.com>
2025-04-17 12:56:21 +0200

    [#28][#29] - Crear test para register.php

---
+++ analytics/tests/Unit/registerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+include_once "analytics/src/register.php";
+
+class RegisterTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function givenValidEmailReturns1()
+    {
+        $this->assertEquals(1, comprobarEmail("test@test.com"));
+    }
+}
