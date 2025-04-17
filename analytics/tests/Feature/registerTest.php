823f05be08c030306852b610c37a02ad698e69a5
OierBaigo <oierbaigo@gmail.com>
2025-04-17 13:03:36 +0200

    [#28][#29] - Test dado email correcto devuelve api_key

--- analytics/tests/Feature/registerTest.php
+++ analytics/tests/Feature/registerTest.php
@@ -59,4 +59,33 @@ class RegisterTest extends TestCase
         $this->assertEquals(400, $httpCode);
         $this->assertEquals($expectedResponseData, $httpResponseData);
     }
+    /**
+     * @test
+     */
+    public function givenValidEmailReturnsApiKey(): void
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, "https://vyvbts.com/register");
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $email = json_encode([
+            "email" => "holaquetal@gmail.com"
+        ]);
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $email);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($email)
+        ]);
+
+        $httpResponse = curl_exec($curl);
+        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+
+        $httpResponseData = json_decode($httpResponse, true);
+        //$expectedResponseData = ["api_key" => ""];
+
+        $this->assertEquals(200, $httpCode);
+        $this->assertArrayHasKey("api_key", $httpResponseData);
+        //$this->assertEquals($expectedResponseData, $httpResponseData);
+    }
 }
