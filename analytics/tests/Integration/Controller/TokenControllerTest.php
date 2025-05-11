fb523ff637a11fecdce908a5db0091d55eff549d
AlexSolaAlba <149529379+AlexSolaAlba@users.noreply.github.com>
2025-05-11 19:07:46 +0200

    [#49][#53] - refactorizar token (#65)
    * [#49][#53] - Creacion test devuelve 400 cuando no hay email
    
    * [#49][#53] - Creacion test devuelve 400 cuando el email es incorrecto
    
    * [#49][#53] - Creacion test devuelve 400 cuando el email es correcto y la key falta
    
    * [#49][#53] - Creacion test devuelve 400 cuando el email es correcto y la key es incorrecta
    
    * [#49][#53] - Creacion test devuelve 200 cuando el email y la key son correctos

---
+++ analytics/tests/Integration/Controller/TokenControllerTest.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Integration\Controller;
+
+use Laravel\Lumen\Testing\TestCase;
+use Mockery;
+use Illuminate\Http\Request;
+use Random\RandomException;
+use TwitchAnalytics\Controllers\Token\TokenController;
+use TwitchAnalytics\Controllers\Token\TokenValidator;
+use TwitchAnalytics\Domain\DB\DataBaseHandler;
+use TwitchAnalytics\Domain\Key\RandomKeyGenerator;
+
+class TokenControllerTest extends TestCase
+{
+    public function createApplication()
+    {
+        return require __DIR__ . '/../../../bootstrap/app.php';
+    }
+    private TokenController $tokenController;
+
+    /**
+     * @throws RandomException
+     * @SuppressWarnings(PHPMD.StaticAccess)
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $keyGenerator = Mockery::mock(RandomKeyGenerator::class);
+        $keyGenerator->allows()->generateRandomKey()->andReturns("24e9a3dea44346393f632e4161bc83e6");
+        $dataBaseHandler = new DatabaseHandler();
+        $tokenValidator = new TokenValidator();
+        $this->tokenController = new TokenController($dataBaseHandler, $tokenValidator, $keyGenerator);
+    }
+
+    /**
+     * @test
+     * @SuppressWarnings(PHPMD.StaticAccess)
+     */
+    public function gets400WhenEmailParameterIsMissing(): void
+    {
+        $request = Request::create('/token', 'POST');
+
+        $response = $this->tokenController->__invoke($request);
+
+        $this->assertEquals(400, $response->getStatusCode());
+        $this->assertEquals([
+            'error' => 'The email is mandatory'
+        ], $response->getData(true));
+    }
+
+    /**
+     * @test
+     * @SuppressWarnings(PHPMD.StaticAccess)
+     */
+    public function gets400WhenEmailParameterIsWrong(): void
+    {
+        $request = Request::create('/token', 'POST', [
+            'email' => 'testexample.com'
+        ]);
+
+        $response = $this->tokenController->__invoke($request);
+
+        $this->assertEquals(400, $response->getStatusCode());
+        $this->assertEquals([
+            'error' => 'The email must be a valid email address'
+        ], $response->getData(true));
+    }
+
+    /**
+     * @test
+     * @SuppressWarnings(PHPMD.StaticAccess)
+     */
+    public function gets400WhenEmailParameterIsRightAndKeyParameterIsMissing(): void
+    {
+        $request = Request::create('/token', 'POST', [
+            'email' => 'test@example.com'
+        ]);
+
+        $response = $this->tokenController->__invoke($request);
+
+        $this->assertEquals(400, $response->getStatusCode());
+        $this->assertEquals([
+            'error' => 'The key is mandatory'
+        ], $response->getData(true));
+    }
+
+    /**
+     * @test
+     * @SuppressWarnings(PHPMD.StaticAccess)
+     */
+    public function gets400WhenEmailParameterIsRightAndKeyParameterIsWrong(): void
+    {
+        $request = Request::create('/token', 'POST', [
+            'email' => 'test@example.com',
+            'api_key' => '21343fse'
+        ]);
+
+        $response = $this->tokenController->__invoke($request);
+
+        $this->assertEquals(400, $response->getStatusCode());
+        $this->assertEquals([
+            'error' => 'The key must be a valid key'
+        ], $response->getData(true));
+    }
+
+    /**
+     * @test
+     * @SuppressWarnings(PHPMD.StaticAccess)
+     */
+    public function gets200WhenEmailParameterIsRightAndKeyParameterIsRight(): void
+    {
+        $request = Request::create('/token', 'POST', [
+            'email' => 'test@example.com',
+            'api_key' => '24e9a3dea44346393f632e4161bc83e6'
+        ]);
+
+        $response = $this->tokenController->__invoke($request);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(['token' => "24e9a3dea44346393f632e4161bc83e6"], $response->getData(true));
+    }
+}
