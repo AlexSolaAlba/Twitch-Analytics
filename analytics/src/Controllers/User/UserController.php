33fdb7668ff48b812aa62e8e8a523c270735fe0c
OierBaigo <oierbaigo@gmail.com>
2025-05-17 19:39:16 +0200

    [#49][#55] - Separacion de la logica de controller a service

--- analytics/src/Controllers/User/UserController.php
+++ analytics/src/Controllers/User/UserController.php
@@ -6,11 +6,10 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 use TwitchAnalytics\Application\Services\RefreshTwitchTokenService;
+use TwitchAnalytics\Application\Services\UserService;
 use TwitchAnalytics\Controllers\ValidationException;
 use TwitchAnalytics\Domain\Exceptions\ApiKeyException;
 use TwitchAnalytics\Domain\Repositories\UserRepository\UserRepositoryInterface;
-use TwitchAnalytics\Infraestructure\ApiStreamer\ApiStreamerInterface;
-use TwitchAnalytics\Infraestructure\DB\DataBaseHandler;
 use TwitchAnalytics\Infraestructure\Exceptions\NotFoundException;
 
 class UserController extends BaseController
@@ -18,20 +17,17 @@ class UserController extends BaseController
     private RefreshTwitchTokenService $refreshTwitchToken;
     private UserValidator $userValidator;
     private UserRepositoryInterface $userRepository;
-    private DataBaseHandler $dataBaseHandler;
-    private ApiStreamerInterface $apiStreamer;
+    private UserService $userService;
     public function __construct(
         RefreshTwitchTokenService $refreshTwitchToken,
         UserValidator $userValidator,
         UserRepositoryInterface $userRepository,
-        DataBaseHandler $dataBaseHandler,
-        ApiStreamerInterface $apiStreamer
+        UserService $userService
     ) {
         $this->refreshTwitchToken = $refreshTwitchToken;
         $this->userValidator = $userValidator;
         $this->userRepository = $userRepository;
-        $this->dataBaseHandler = $dataBaseHandler;
-        $this->apiStreamer = $apiStreamer;
+        $this->userService = $userService;
     }
 
     public function __invoke(Request $request): JsonResponse
@@ -42,7 +38,7 @@ class UserController extends BaseController
             $tokenUser = $this->userValidator->validateToken($request->header('Authorization'));
             $user = $this->userRepository->verifyUserToken($tokenUser);
 
-            return response()->json($this->returnStreamerInfo($request->get('id'), $user->getToken()));
+            return response()->json($this->userService->returnStreamerInfo($request->get('id'), $user->getToken()));
         } catch (ApiKeyException $ex) {
             return response()->json(['error' => $ex->getMessage()], 401);
         } catch (ValidationException $ex) {
@@ -51,54 +47,6 @@ class UserController extends BaseController
             return response()->json(['error' => $ex->getMessage()], 404);
         } catch (\Throwable $ex) {
             return response()->json(['error' => $ex->getMessage()], 500);
-        }
-    }
-    /**
-     * @SuppressWarnings(PHPMD.ElseExpression)
-     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
-     */
-    private function returnStreamerInfo($userId, $accessToken): array
-    {
-        $connection = $this->dataBaseHandler->connectWithDB();
-        $this->dataBaseHandler->checkConnection($connection);
-
-        try {
-            $stmt = $connection->prepare("SELECT * FROM usersTwitch where id = ?");
-            $stmt->bind_param("i", $userId);
-            $this->dataBaseHandler->checkStmtExecution($stmt);
-            $streamerRaw = $stmt->get_result();
-
-            if ($streamerRaw->num_rows > 0) {
-                $streamer = $streamerRaw->fetch_assoc();
-                return [
-                    "id" => $streamer["id"],
-                    "login" => $streamer["user_login"],
-                    "display_name" => $streamer["display_name"],
-                    "type" => $streamer["user_type"],
-                    "broadcaster_type" => $streamer["broadcaster_type"],
-                    "description" => $streamer["user_description"],
-                    "profile_image_url" => $streamer["profile_image_url"],
-                    "offline_image_url" => $streamer["offline_image_url"],
-                    "view_count" => $streamer["view_count"],
-                    "created_at" => $streamer["created_at"]
-                ];
-            } else {
-                $streamer = $this->apiStreamer->getStreamerFromTwitch($userId, $accessToken, $this->dataBaseHandler);
-                return [
-                    "id" => $streamer->getStreamerId(),
-                    "login" => $streamer->getLogin(),
-                    "display_name" => $streamer->getDisplayName(),
-                    "type" => $streamer->getType(),
-                    "broadcaster_type" => $streamer->getBroadcasterType(),
-                    "description" => $streamer->getDescription(),
-                    "profile_image_url" => $streamer->getProfileImageUrl(),
-                    "offline_image_url" => $streamer->getOfflineImageUrl(),
-                    "view_count" => $streamer->getViewCount(),
-                    "created_at" => $streamer->getCreatedAt()
-                ];
-            }
-        } finally {
-            $connection->close();
         }
     }
 }
