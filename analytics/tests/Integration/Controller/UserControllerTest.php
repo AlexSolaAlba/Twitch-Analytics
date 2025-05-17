86efe3305d7f2761accbbf725eb8d962440e7a2b
OierBaigo <oierbaigo@gmail.com>
2025-05-17 19:25:13 +0200

    [#49][#55] - Test de integracion token buneo no funciona

--- analytics/tests/Integration/Controller/UserControllerTest.php
+++ analytics/tests/Integration/Controller/UserControllerTest.php
@@ -7,7 +7,7 @@ use Laravel\Lumen\Testing\TestCase;
 use TwitchAnalytics\Application\Services\RefreshTwitchTokenService;
 use TwitchAnalytics\Controllers\User\UserController;
 use TwitchAnalytics\Controllers\User\UserValidator;
-use TwitchAnalytics\Infraestructure\ApiClient\ApiTwitchClient;
+use TwitchAnalytics\Infraestructure\ApiClient\FakeApiTwitchClient;
 use TwitchAnalytics\Infraestructure\ApiStreamer\ApiStreamer;
 use TwitchAnalytics\Infraestructure\DB\DataBaseHandler;
 use TwitchAnalytics\Infraestructure\Repositories\TwitchUserRepository;
@@ -30,8 +30,8 @@ class UserControllerTest extends TestCase
     {
         parent::setUp();
         $dataBaseHandler = new DataBaseHandler();
-        $apiTwitchClient = new ApiTwitchClient();
-        $twitchUserRepository = new TwitchUserRepository($dataBaseHandler, $apiTwitchClient);
+        $fakeApiTwitchClient = new FakeApiTwitchClient();
+        $twitchUserRepository = new TwitchUserRepository($dataBaseHandler, $fakeApiTwitchClient);
         $timeProvider = new SystemTimeProvider();
         $refreshTwitchToken = new RefreshTwitchTokenService($twitchUserRepository, $timeProvider);
         $userValidator = new UserValidator();
@@ -137,6 +137,35 @@ class UserControllerTest extends TestCase
         $this->assertEquals(401, $response->getStatusCode());
         $this->assertEquals([
             'error' => 'Unauthorized. Token is invalid or expired.'
+        ], $response->getData(true));
+    }
+
+    /**
+     * @test
+     * @SuppressWarnings(PHPMD.StaticAccess)
+     */
+    public function givenStreamerIdThatExistsInDBReturnsStreamerInfo()
+    {
+        $request = Request::create('/user', 'GET', [
+            'id' => 1,
+        ], [], [], [
+            'HTTP_Authorization' => 'Bearer 24e9a3dea44346393f632e4161bc83e6',
+        ]);
+
+        $response = $this->userController->__invoke($request);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals([
+            'id' => '1',
+            'login' => 'elsmurfoz',
+            'display_name' => 'elsmurfoz',
+            'type' => '',
+            'broadcaster_type' => '',
+            'description' => '',
+            'profile_image_url' => 'https://static-cdn.jtvnw.net/user-default-pictures-uv/215b7342-def9-11e9-9a66-784f43822e80-profile_image-300x300.png',
+            'offline_image_url' => '',
+            'view_count' => '0',
+            'created_at' => '2007-05-22T10:37:47Z',
         ], $response->getData(true));
     }
 }
