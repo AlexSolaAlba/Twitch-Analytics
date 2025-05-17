828d4840dc2def1843acfa8c88b04a4420cc5056
OierBaigo <149527397+OierBaigo@users.noreply.github.com>
2025-05-17 19:44:05 +0200

    [#49][#55] - refactorizar user (#83)
    * [#49][#55] - Separacion de la logica de controller a service
    
    * [#49][#55] - Refactor test para el service

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-05-2025 a las 18:43:16
+-- Tiempo de generación: 17-05-2025 a las 19:31:12
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `twitch_analytics`
+-- Base de datos: `twitch-analytics`
 --
 
 -- --------------------------------------------------------
@@ -40,7 +40,7 @@ CREATE TABLE `token` (
 --
 
 INSERT INTO `token` (`tokenID`, `accessToken`, `tokenExpire`, `clientId`, `clientSecret`) VALUES
-(1, 'tb9rmjd4bo2ldoru98wogmfaclvrng', 1750831928, 'mqf0orb9t2ufb34nhd3em686qpb8xc', 'ko7q3si9erixwp84l9w2j9zrdowquh');
+(1, 'jostpf5q0puzmxmkba9iyug38kjtg', 1752514242, 'mqf0orb9t2ufb34nhd3em686qpb8xc', 'ko7q3si9erixwp84l9w2j9zrdowquh');
 
 -- --------------------------------------------------------
 
@@ -51,7 +51,7 @@ INSERT INTO `token` (`tokenID`, `accessToken`, `tokenExpire`, `clientId`, `clien
 CREATE TABLE `topscache` (
   `game_id` int(11) NOT NULL,
   `game_name` varchar(100) NOT NULL,
-  `user_name` varchar(100) NOT NULL,
+  `user_name` int(11) NOT NULL,
   `total_videos` int(11) NOT NULL,
   `total_views` int(11) NOT NULL,
   `most_viewed_title` varchar(100) NOT NULL,
@@ -66,9 +66,9 @@ CREATE TABLE `topscache` (
 --
 
 INSERT INTO `topscache` (`game_id`, `game_name`, `user_name`, `total_videos`, `total_views`, `most_viewed_title`, `most_viewed_views`, `most_viewed_duration`, `most_viewed_created_at`, `created_at`) VALUES
-(509658, 'Just Chatting', 'KaiCenat', 36, 453731096, '🦃 MAFIATHON 2 🦃 KAI X KEVIN HART X DRUSKI 🦃 DAY 27 🦃 20% OF REVENUE GOING TO SCHOOL IN NIGERIA 🦃 ALL', 24911112, '22h5m32s', '2024-11-28T02:06:07Z', '2025-05-08 19:18:05'),
-(32982, 'Grand Theft Auto V', 'らっだぁ', 34, 43221895, '【VCR2日目】初めての犯罪！ 鳩禁指示禁', 2116977, '13h27m6s', '2023-12-11T04:23:05Z', '2025-05-08 19:18:05'),
-(29595, 'Dota 2', 'dota2ti_ru', 21, 148501050, '[RU] Team Secret vs Team Spirit | Основной этап | The International 10 | День 6', 17072719, '12h45m1s', '2021-10-17T05:57:42Z', '2025-05-08 19:18:05');
+(516575, 'VALORANT', 0, 3, 7705401, 'V a l o r a n t Grind begins | Among us tonight at 8 CENTRAL with a SOLID crew! ', 4549589, '15h15m21s', '2020-09-11T18:52:09Z', '2025-02-20 11:36:15'),
+(21779, 'League of Legends', 0, 26, 124973951, 'WORLDS 22 FINALS COUNTDOWN', 11620705, '9h25m12s', '2022-11-05T21:00:23Z', '2025-02-20 11:36:15'),
+(509658, 'Just Chatting', 0, 36, 414862770, '🦃 MAFIATHON 2 🦃 KAI X KEVIN HART X DRUSKI 🦃 DAY 27 🦃 20% OF REVENUE GOING TO SCHOOL IN NIGERIA 🦃 ALL', 24869941, '22h5m32s', '2024-11-28T02:06:07Z', '2025-02-20 11:36:15');
 
 -- --------------------------------------------------------
 
@@ -89,11 +89,11 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`userID`, `userEmail`, `userApiKey`, `userToken`, `userTokenExpire`) VALUES
-(1, 'holaquetal22@gmail.com', '8ebf2dd9e613b068ae3cfbdffbeb730f', '09141422ea9df1950069536db36caa0b', 1747251055),
-(2, 'holaquetal@gmail.com', '495c04d1a065ecff8cb21dc218674d79', '5b0d2de4530b154647a92e714e03d4e7', 1740497735),
-(3, 'usuario@example.com', '91bc1f3dec5152f9a4c30e938008abad', 'd0908e2c4460996281fd6fe87288cbf6', 1746776805),
-(4, 'usuariooo@example.com', '07a48053d48178ba98de51cfefebab70', '', 0),
-(5, 'test@example.com', '24e9a3dea44346393f632e4161bc83e6', '24e9a3dea44346393f632e4161bc83e6', 1747248356);
+(1, 'holaquetal22@gmail.com', '8ebf2dd9e613b068ae3cfbdffbeb730f', 'b5d92c801a4847e73895135d4614a1aa', 1741536957),
+(2, 'holaquetal@gmail.com', '74e365a90fb4b7e129070418d4598a62', '5b0d2de4530b154647a92e714e03d4e7', 1740497735),
+(3, 'ghdjs@gmail.com', 'fb514f2384d96403b50e65723b1745ac', '', 0),
+(4, 'test@example.com', '24e9a3dea44346393f632e4161bc83e6', '24e9a3dea44346393f632e4161bc83e6', 1747762171),
+(5, 'testcaducado@gmail.com', 'e9cb15bba53c9d05a23c21afc7b44f40', 'e9cb15bba53c9d05a23c21afc7b44f40', 0);
 
 -- --------------------------------------------------------
 
@@ -120,7 +120,6 @@ CREATE TABLE `userstwitch` (
 
 INSERT INTO `userstwitch` (`id`, `user_login`, `display_name`, `user_type`, `broadcaster_type`, `user_description`, `profile_image_url`, `offline_image_url`, `view_count`, `created_at`) VALUES
 ('1', 'elsmurfoz', 'elsmurfoz', '', '', '', 'https://static-cdn.jtvnw.net/user-default-pictures-uv/215b7342-def9-11e9-9a66-784f43822e80-profile_image-300x300.png', '', '0', '2007-05-22T10:37:47Z'),
-('1234', 'zdraste_vladkenov', 'zdraste_vladkenov', '', '', 'wasde876', 'https://static-cdn.jtvnw.net/user-default-pictures-uv/ebe4cd89-b4f4-4cd9-adac-2f30151b4209-profile_image-300x300.png', '', '0', '2018-09-04T15:23:04Z'),
 ('2', 'goyabean', 'goyabean', '', '', '', 'https://static-cdn.jtvnw.net/user-default-pictures-uv/294c98b5-e34d-42cd-a8f0-140b72fba9b0-profile_image-300x300.png', '', '0', '2007-05-22T10:37:47Z'),
 ('3', 'djracerx', 'djracerx', '', '', '', 'https://static-cdn.jtvnw.net/user-default-pictures-uv/998f01ae-def8-11e9-b95c-784f43822e80-profile_image-300x300.png', '', '0', '2007-05-22T10:37:47Z');
 
