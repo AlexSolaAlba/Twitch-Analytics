1152984c8e25ef45a47af37e88082c23e711ea64
EthanTurrillas <ethanturrillas8@gmail.com>
2025-05-23 18:26:40 +0200

    [#49][#56] - Test de integración de authorization no existe

--- analytics/tests/Integration/Controller/EnrichedControllerTest.php
+++ analytics/tests/Integration/Controller/EnrichedControllerTest.php
@@ -96,4 +96,23 @@ class EnrichedControllerTest extends TestCase
             'error' => 'Invalid or missing limit parameter.'
         ], $response->getData(true));
     }
+    /**
+     * @test
+     * @SuppressWarnings(PHPMD.StaticAccess)
+     */
+    public function givenTokenThatNotExistsReturnsAnException()
+    {
+        $request = Request::create('/streams/enriched', 'GET', [
+            'limit' => 2,
+        ], [], [], [
+            'HTTP_Authorization' => 'Bearer ',
+        ]);
+
+        $response = $this->enrichedController->__invoke($request);
+
+        $this->assertEquals(401, $response->getStatusCode());
+        $this->assertEquals([
+            'error' => 'Unauthorized. Token is invalid or expired.'
+        ], $response->getData(true));
+    }
 }
