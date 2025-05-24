6a893f02b59407203ed36f398f16a0d2fbf1e96d
Mikelats <mikelatsito@gmail.com>
2025-05-24 20:16:12 +0200

    [#49][#54] - Test de integración de since y token válido

--- analytics/tests/Integration/Controller/TopsOfTheTopsControllerTest.php
+++ analytics/tests/Integration/Controller/TopsOfTheTopsControllerTest.php
@@ -136,4 +136,44 @@ class TopsOfTheTopsControllerTest extends TestCase
             'error' => 'Unauthorized. Token is invalid or expired.'
         ], $response->getData(true));
     }
+
+    /**
+     * @test
+     * @SuppressWarnings(PHPMD.StaticAccess)
+     */
+    public function givenGoodSinceAndTokenReturnsTopsInfo()
+    {
+        $request = Request::create('/topsofthetops', 'GET', [
+            'since' => 50,
+        ], [], [], [
+            'HTTP_Authorization' => 'Bearer 3d211f8be2f71ebd34ab2707d90c935f',
+        ]);
+
+        $response = $this->topsController->__invoke($request);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals([
+        [
+            'game_id' => 509658,
+            'game_name' => 'Just Chatting',
+            'user_name' => 'Kai Cenat',
+            'total_videos' => 36,
+            'total_views' => 414857711,
+            'most_viewed_title' => '🦃 MAFIATHON 2 🦃 KAI X KEVIN HART X DRUSKI 🦃 DAY 27 🦃 20% OF REVENUE GOING TO SCHOOL IN NIGERIA 🦃 ALL',
+            'most_viewed_views' => 24868821,
+            'most_viewed_duration' => '22h5m32s',
+            'most_viewed_created_at' => '2024-11-28T02:06:07Z'
+        ],
+        [
+            'game_id' => 516575,
+            'game_name' => 'VALORANT',
+            'user_name' => '0',
+            'total_videos' => 3,
+            'total_views' => 7705399,
+            'most_viewed_title' => 'V a l o r a n t Grind begins | Among us tonight at 8 CENTRAL with a SOLID crew!',
+            'most_viewed_views' => 4549587,
+            'most_viewed_duration' => '15h15m21s',
+            'most_viewed_created_at' => '2020-09-11T18:52:09Z'
+        ]
+        ], $response->getOriginalContent());
+    }
 }
