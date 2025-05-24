cba2048b5687d222e498c3ba2dd8fab79f22e734
Mikelats <mikelatsito@gmail.com>
2025-05-24 20:35:09 +0200

    [#49][#54] - Correción de errores en app

--- analytics/bootstrap/app.php
+++ analytics/bootstrap/app.php
@@ -63,16 +63,6 @@ $app->singleton(
 );
 
 $app->singleton(
-    TwitchAnalytics\Infraestructure\ApiClient\ApiTwitchVideos\ApiTwitchVideosInterface::class,
-    TwitchAnalytics\Infraestructure\ApiClient\ApiTwitchVideos\FakeApiTwitchVideos::class
-);
-
-$app->singleton(
-    TwitchAnalytics\Infraestructure\ApiClient\ApiTwitchEnriched\ApiTwitchEnrichedInterface::class,
-    TwitchAnalytics\Infraestructure\ApiClient\ApiTwitchEnriched\FakeApiTwitchEnriched::class
-);
-
-$app->singleton(
     TwitchAnalytics\Domain\Time\TimeProviderInterface::class,
     TwitchAnalytics\Infraestructure\Time\SystemTimeProvider::class
 );
