16f40dca796df43238caf2918cca937940401f58
Mikelats <145489452+MikelatsPerezdeIriarte@users.noreply.github.com>
2025-05-25 00:14:26 +0200

    [#49][#54] - Cambio de token en tests de topsofthetops (#120)

--- analytics/bootstrap/app.php
+++ analytics/bootstrap/app.php
@@ -170,7 +170,7 @@ $app->singleton(
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\Handler::class
+    Laravel\Lumen\Exceptions\Handler::class
 );
 
 $app->singleton(
