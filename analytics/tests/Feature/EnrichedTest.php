44568b29c79a416afeebb29c9be77f05d897e5a1
Alejandro Sola <alejandrosolaalba@gmail.com>
2025-06-02 13:32:08 +0200

    [#125][#133] - Test devuelve 401 si no hay token

--- analytics/tests/Feature/EnrichedTest.php
+++ analytics/tests/Feature/EnrichedTest.php
@@ -27,4 +27,21 @@ class EnrichedTest extends TestCase
             ]
         );
     }
+
+    /**
+     * @test
+     */
+    public function gets401WhenTokenIsNotGiven()
+    {
+        $response = $this->get('/analytics/streams/enriched?limit=2', [
+            'Authorization' => '',
+        ]);
+
+        $response->assertResponseStatus(401);
+        $response->seeJson(
+            [
+                'error' => 'Unauthorized. Token is invalid or expired.'
+            ]
+        );
+    }
 }
