a3693b52c52d78b4c1e38a172fcaaa3b2b9b782f
OierBaigo <oierbaigo@gmail.com>
2025-06-02 12:05:20 +0200

    [#125][#130] - Test register con buen email

--- analytics/tests/Feature/RegisterTest.php
+++ analytics/tests/Feature/RegisterTest.php
@@ -3,6 +3,9 @@
 namespace Feature;
 
 use Laravel\Lumen\Testing\TestCase;
+use Mockery;
+use Random\RandomException;
+use TwitchAnalytics\Domain\Key\RandomKeyGenerator;
 
 class RegisterTest extends TestCase
 {
@@ -39,6 +42,29 @@ class RegisterTest extends TestCase
         $response->seeJson(
             [
                 'error' => 'The email must be a valid email address'
+            ]
+        );
+    }
+
+    /**
+     * @test
+     * @throws RandomException
+     * @SuppressWarnings(PHPMD.StaticAccess)
+     */
+    public function gets200WhenEmailParameterIsRight(): void
+    {
+        $keyGenerator = Mockery::mock(RandomKeyGenerator::class);
+        $keyGenerator->allows()->generateRandomKey()->andReturns("24e9a3dea44346393f632e4161bc83e6");
+        $this->app->instance(RandomKeyGenerator::class, $keyGenerator);
+
+        $response = $this->post('/register', [
+            'email' => 'test@example.com'
+        ]);
+
+        $response->assertResponseStatus(200);
+        $response->seeJson(
+            [
+                'api_key' => '24e9a3dea44346393f632e4161bc83e6'
             ]
         );
     }
