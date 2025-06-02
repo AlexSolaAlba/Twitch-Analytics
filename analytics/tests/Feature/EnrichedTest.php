0838339f48f277856799c10715f1a06f5e3e0f68
Alejandro Sola <alejandrosolaalba@gmail.com>
2025-06-02 13:29:49 +0200

    [#125][#133] - Test devuelve 401 con token incorrecto

---
+++ analytics/tests/Feature/EnrichedTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace TwitchAnalytics\Tests\Feature;
+
+use Laravel\Lumen\Testing\TestCase;
+
+class EnrichedTest extends TestCase
+{
+    public function createApplication()
+    {
+        return require __DIR__ . '/../../bootstrap/app.php';
+    }
+
+    /**
+     * @test
+     */
+    public function gets401WhenTokenIsWrong(): void
+    {
+        $response = $this->get('/analytics/streams/enriched?limit=2', [
+            'Authorization' => 'Bear',
+        ]);
+
+        $response->assertResponseStatus(401);
+        $response->seeJson(
+            [
+                'error' => 'Unauthorized. Token is invalid or expired.'
+            ]
+        );
+    }
+}
