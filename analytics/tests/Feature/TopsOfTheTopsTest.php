5abb25e3ee88840aadf1002645f7c88d38276216
Mikelats <mikelatsito@gmail.com>
2025-06-02 13:05:56 +0200

    [#125][#135] - Test de tops con token inexistente

--- analytics/tests/Feature/TopsOfTheTopsTest.php
+++ analytics/tests/Feature/TopsOfTheTopsTest.php
@@ -36,18 +36,14 @@ class TopsOfTheTopsTest extends TestCase
      */
     public function notGivenTokenReturnsAnException()
     {
-        $request = Request::create('/topsofthetops', 'GET', [
-            'since' => 50,
-        ], [], [], [
-            'HTTP_Authorization' => '',
+        $response = $this->get('/analytics/topsofthetops?since=50', [
+            'HTTP_Authorization' => ''
         ]);
 
-        $response = $this->topsController->__invoke($request);
-
-        $this->assertEquals(401, $response->getStatusCode());
-        $this->assertEquals([
+        $response->assertResponseStatus(401);
+        $response->seeJson([
             'error' => 'Unauthorized. Token is invalid or expired.'
-        ], $response->getData(true));
+        ]);
     }
 
     /**
